ef376e1a126db7f22347ac41e5611198fc6f0b20
Maria Berestovaya <57846761+antaresofficial@users.noreply.github.com>
2022-12-29 03:50:58 +0500

    Added global guard.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
-import { CertHeaderStrategy } from './cert.strategy';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CertHeaderStrategy } from './strategies/cert.strategy';
 
 @Module({
   controllers: [AppController],
-  providers: [CertHeaderStrategy],
+  providers: [
+    CertHeaderStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: LocalAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
